6e03a22aa0ec8b7af37f6c8b114a23fb4de7c842
Fernando <fernandostarlin1012@gmail.com>
2023-02-24 11:52:25 -0400

    Single Source of truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,14 @@ export class Counters extends Component {
         ]
     };
 
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0
+            return c;
+        });
+        this.setState({counters});
+    }
+
     handleDelete = counterId => {
         const counters = this.state.counters.filter(c => c.id !== counterId );
         this.setState({counters});
@@ -20,6 +28,9 @@ export class Counters extends Component {
     render() {
         return (
             <div>
+                <button
+                    onClick={this.handleReset}
+                    className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.state.counters.map(counter =>
                     <Counter key={counter.id}
                              onDelete={this.handleDelete}
