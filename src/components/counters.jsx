6032c42037be139b639f7ff940f1a45426ca29e0
Fernando <fernandostarlin1012@gmail.com>
2023-02-24 11:39:07 -0400

    Updating the state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,22 +4,26 @@ import Counter from "./counter";
 export class Counters extends Component {
     state = {
         counters: [
-            {id: 1, value: 0},
+            {id: 1, value: 4},
             {id: 2, value: 0},
             {id: 3, value: 0},
             {id: 4, value: 0},
         ]
     };
 
-    handleDelete() {
-        console.log('Event Handler clicked')
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter(c => c.id !== counterId );
+        this.setState({counters});
+        console.log('Event Handler clicked', counterId);
     }
 
     render() {
         return (
             <div>
                 {this.state.counters.map(counter =>
-                    <Counter Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id}/>)}
+                    <Counter key={counter.id}
+                             onDelete={this.handleDelete}
+                             counter={counter}/>)}
             </div>
         );
     }
