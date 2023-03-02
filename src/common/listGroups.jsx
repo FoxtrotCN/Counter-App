9f0af5168fc28a957f03c93b4d772f0660946fdc
Fernando <fernandostarlin1012@gmail.com>
2023-03-02 18:26:55 -0400

    We've started to work with the list group of genres component for filtering it.

---
+++ src/common/listGroups.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const ListGroups = () => {
+    return (
+        <ul className="list-group">
+            <li className="list-group-item">All ergh</li>
+            <li className="list-group-item">rgrgg</li>
+            <li className="list-group-item">svgasgsdfg</li>
+            <li className="list-group-item">sdasgag</li>
+        </ul>
+    )
+}
+
+export default ListGroups;
