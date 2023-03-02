5b8b10c05268666e139ae922ba02147e4dd17bbf
Fernando <fernandostarlin1012@gmail.com>
2023-03-02 16:18:48 -0400

    Starting to work with Pagination, Filtering and Sorting. This time, we've started with the pagination component.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 import {getMovies} from "../services/fakeMovieService";
 import Like from "../common/like";
+import Pagination from "../common/pagination";
 
 
 export class Movie extends Component {
     state = {
-        movies: getMovies()
+        movies: getMovies(),
+        pageSize: 4,
     };
 
     handleDelete = (movie) => {
@@ -20,6 +22,10 @@ export class Movie extends Component {
         movies[index].liked = !movies[index].liked;
         this.setState({movies});
     };
+
+    handlePageChange = page => {
+        console.log(page);
+    }
 
     render() {
         const {length: count} = this.state.movies;
@@ -61,6 +67,7 @@ export class Movie extends Component {
                     ))}
                     </tbody>
                 </table>
+                <Pagination itemsCount={count} pageSize={this.state.pageSize} onPageChange={this.handlePageChange}/>
             </React.Fragment>
         );
     }
